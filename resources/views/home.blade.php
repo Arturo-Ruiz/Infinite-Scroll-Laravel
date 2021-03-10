66deae58d0d0fc867162003438c3a1982775f981
Arturo <gregory09ruiz@gmail.com>
2021-03-10 15:39:10 -0400

    home.blade ready and created pagination.blade users

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,11 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+    <div class="col-8" id="container">
+        @include('users.pagination')
+    </div>
+    <div id="loading" hidden>
+        <h1>Loading...</h1>
     </div>
 </div>
 @endsection
