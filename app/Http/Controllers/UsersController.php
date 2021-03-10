f51670f0b77efed19b5655b9e715c73ca40b01dd
Arturo <gregory09ruiz@gmail.com>
2021-03-10 15:34:26 -0400

    created functions

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //
+
+    public function index(){
+
+    }
+
+    public function pagination(){
+        
+    }
+
 }
